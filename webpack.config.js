f78a1bef4ad7eb13630e216e07bbf696cb30b429
Travis Benton <travisryanbenton@gmail.com>
2017-02-04 22:41:08 -0600

    tabs to spaces add linting

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-	entry: path.join(__dirname, '/app/scripts/main'),
-	output: {
-		path: path.join(__dirname, '/build/'),
-		filename: 'index.js'
-	},
-	module: {
-		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			},
+  entry: path.join(__dirname, '/app/scripts/main'),
+  output: {
+    path: path.join(__dirname, '/build/'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
       {
         test: /\.scss$/,
         loaders: ['style-loader', 'css-loader', 'sass-loader']
       }
-		]
-	},
-	plugins: [
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false,
-			},
-			output: {
-				comments: false,
-			}
-		})
-	],
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    })
+  ],
   devtool: 'inline-source-map'
 };
